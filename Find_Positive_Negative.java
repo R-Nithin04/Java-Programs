a4d7e845e6e05007e11a2e83786b35ac9c26b61c
Nithin-Logic <rnithin10172004>
2026-07-22 15:54:48 +0530

    Add Find_Positive_Negative Java program

---
+++ Find_Positive_Negative.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Find_Positive_Negative 
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of n::");
+		int n=sc.nextInt();
+		if(n<0)
+		{
+			System.out.println("n=> "+n+" Is a Negitive Number");
+		}
+		else if(n>0)
+		{
+			System.out.println("n=> "+n+" Is a Positive Number");
+		}
+		else
+		{
+			System.out.println("N is Zero");
+		}
+		
+		//OR USING TERNARY OPERATOR
+		
+		System.out.println(n<0?n+" Is Negative":n>0?n+" Is Positive":n+"Is Zero");
+		sc.close();
+	}
+}
