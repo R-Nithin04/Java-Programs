a8ef68029454e4472158a75ed91a3363215c14bc
Nithin R <rnithin10172004@gmail.com>
2026-07-31 21:28:07 +0530

    Update Task2.java

--- Collections/add/Task2.java
+++ Collections/add/Task2.java
@@ -1,4 +1,14 @@
-
-public class Task2 {
-
+import java.util.*;
+public class Task2 
+{
+	public static void main(String args[])
+	{
+		List l=new ArrayList();
+		l.add("JAVA");
+		l.add("PYTHON");
+		l.add("R STUDIO");
+		l.add("C");
+		l.add("C++");
+		System.out.println(l);
+	}
 }
