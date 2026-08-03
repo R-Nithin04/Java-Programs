dc8685bdcd632a01b719718618e9fa23054fdaec
Nithin R <rnithin10172004@gmail.com>
2026-08-03 21:52:59 +0530

    Add Stack(Legacy) Demonstration

---
+++ Collections/StackCol.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class StackCol 
+{
+	public static void main(String args[])
+	{
+		Stack <Integer>st=new Stack<>();
+		
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		
+		System.out.println(st);
+		
+		st.pop();
+		
+		System.out.println(st);
+		
+		System.out.println(st.peek());
+		
+		System.out.println(st.search(2));
+		
+		System.out.println(st.indexOf(1));
+	}
+}
